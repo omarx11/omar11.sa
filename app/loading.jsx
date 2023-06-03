26b96749901111ceb7c2846021f326e8faf40419
omarx11 <igamerxd3@gmail.com>
2023-06-03 23:57:04 +0300

    update 11

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,12 @@
 export default function Loading() {
   return (
-    <div>
-      <h1>Loading.. main page</h1>
-    </div>
-  )
+    <>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-2 text-4xl font-bold"></div>
+        <p className="text-stone-400">
+          Here's some of my personal projects I've worked on recently.
+        </p>
+      </div>
+    </>
+  );
 }
