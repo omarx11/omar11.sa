b0c4a2e5a52142dafa033e132f8be50b86db2000
omarx11 <igamerxd3@gmail.com>
2023-06-18 03:37:17 +0300

    build fix 3

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useNavContext } from "@/app/context/navigation";
+import { useNavContext } from "@/app/context/Navigation";
 import { linksInfo } from "../data/navigation";
 import * as motion from "../lib/useMoition";
 import { useEffect, useState } from "react";
--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Transition from "./components/Transition";
-import NavProvider from "@/app/context/navigation";
+import NavProvider from "@/app/context/Navigation";
 import config from "@/app/data/config";
 
 const recursive = Recursive({ subsets: ["latin"] });
