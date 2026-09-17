c881846a44c70f3018db61a1d09f5099c3002090
omarx11 <igamerxd3@gmail.com>
2026-09-17 19:10:28 +0300

    chore(dependencies): update tailwindcss and postcss plugin configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
     ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
